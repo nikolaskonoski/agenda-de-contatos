b8a1445eb4b6b5405092c7edb39da875a333f5ad
Nikolas Konoski <nikolasmkonoski@gmail.com>
2025-05-05 17:58:53 -0300

    indentation

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let list = localStorage.getItem("mylist");
 const form = document.querySelector("form");
 const ulPeoples = document.querySelector("ul");
 
- // Adiciona o evento de keyup para o campo de pesquisa
+// Adiciona o evento de keyup para o campo de pesquisa
 document.querySelector('#search').addEventListener('keyup', (e) => listing(e.target.value));
 
-if(list) {
+if (list) {
     list = JSON.parse(list);
 } else {
-    list= [];
+    list = [];
 }
 
 listing();
@@ -21,7 +21,7 @@ form.addEventListener('submit', function (e) {
     newPeople.name = this.name.value;
     newPeople.phone = this.phone.value;
     newPeople.email = this.email.value;
-    if (this.id.value !== "" && this.id.value >=0) {
+    if (this.id.value !== "" && this.id.value >= 0) {
         list[this.id.value] = newPeople;
     } else {
         list.push(newPeople);
@@ -38,41 +38,41 @@ form.addEventListener('submit', function (e) {
 /* Aqui fizemos a função listar, que renderiza os contatos na tela, e também fizemos o tratamento do filtro de pesquisa */
 /* A função listar percorre o array de contatos e renderiza na tela, se o filtro for vazio, renderiza todos os contatos, indexof procura o nome buscado, percorrendo o array e fazendo uma validação*/
 
-function listing(filter='') {
-    ulPeoples.innerHTML = ""; 
-    list.forEach((item,key) => {  
-        if (item.name.toUpperCase().indexOf(filter.toUpperCase()) >= 0 || filter =="" ) {
-        
+function listing(filter = '') {
+    ulPeoples.innerHTML = "";
+    list.forEach((item, key) => {
+        if (item.name.toUpperCase().indexOf(filter.toUpperCase()) >= 0 || filter == "") {
+
             line = document.createElement('li');
 
             let s = `<button OnClick="deleted(${key})">Delete</button> 
-                    <button onClick="edit(${key})">Edit</button>` 
-            
+                    <button onClick="edit(${key})">Edit</button>`
+
             line.innerHTML = "Name:" + item.name + "Phone:" + item.phone + "Email:" + item.email + s;
             ulPeoples.appendChild(line);
-    }
-});
+        }
+    });
 
 
 }
 
 function deleted(id) {
-form.reset(); // Clear the form fields
-list.splice(id, 1);
-saveLS();
-listing();
-alert("Contact deleted successfully");
+    form.reset(); // Clear the form fields
+    list.splice(id, 1);
+    saveLS();
+    listing();
+    alert("Contact deleted successfully");
 }
 
-function saveLS(){
-localStorage.setItem("mylist", JSON.stringify(list));
+function saveLS() {
+    localStorage.setItem("mylist", JSON.stringify(list));
 }
 
 function edit(id) {
-form.id.value = id;
-form.name.value = list[id].name;
-form.phone.value = list[id].phone;
-form.email.value = list[id].email;
+    form.id.value = id;
+    form.name.value = list[id].name;
+    form.phone.value = list[id].phone;
+    form.email.value = list[id].email;
 }
 
 
