579b395cad7b39631ef4628b2742a2fa3b962954
Nikolas Konoski <nikolasmkonoski@gmail.com>
2025-05-22 12:43:36 -0300

    new branch dev

---
+++ README.md
@@ -0,0 +1,22 @@
+# Contact List
+
+A simple contact agenda that will be updated as my knowledge grows.  
+This project is part of my learning journey following the teachings of Professor Hallan Neves.
+
+## 📋 Features
+
+- Create contacts  
+- Edit contacts  
+- Delete contacts  
+- Save contacts  
+- Search contacts  
+
+## 🛠️ Technologies Used
+
+- HTML  
+- CSS  
+- JavaScript  
+
+Its functions are to create, edit, delete, save and search the user's contacts.
+
+Link: https://nikolaskonoski.github.io/contacts-list/
---
+++ client/script.js
@@ -0,0 +1,99 @@
+let list = JSON.parse(localStorage.getItem("mylist")) || [];
+
+const form = document.querySelector("form");
+const ulPeoples = document.querySelector("ul");
+const searchInput = document.querySelector('#search');
+
+searchInput.addEventListener('keyup', (e) => listing(e.target.value));
+
+listing();
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    /*Validation of inputs */
+    const nameRegex = /^[A-Za-zÀ-ÿ\s]+$/;
+    const phoneRegex = /^(?=(?:.*\d){8,11})[\d\s()+-]{8,20}$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!nameRegex.test(this.name.value)) {
+        alert("Invalid name. Only letters and spaces are allowed.");
+        return;
+    }
+
+    if (!phoneRegex.test(this.phone.value)) {
+        alert("Invalid phone number. Only digits (8 to 15 characters) are allowed.");
+        return;
+    }
+
+    if (!emailRegex.test(this.email.value)) {
+        alert("Invalid email format.");
+        return;
+    }
+
+    const newPeople = {
+    name: this.name.value,
+    phone: this.phone.value,
+    email: this.email.value
+    };
+
+    const idValue = this.id.value;
+
+    if (idValue !== "" && !isNaN(idValue)) {
+        list[parseInt(idValue)] = newPeople;
+    } else {
+        list.push(newPeople);
+    }
+
+    this.id.value = ""; // Clear the hidden input field
+    this.reset(); // Clears the form fields
+
+    saveContact(); // Saves to local storage
+    listing(); // Updates the contact list on screen
+
+});
+    function listing(filter = '') {
+        ulPeoples.innerHTML = "";
+
+        list.forEach((item, key) => {
+        if (item.name.toUpperCase().includes(filter.toUpperCase()) || filter === "") {
+            const line = document.createElement('li');
+            line.innerHTML = `Name: ${item.name} | Phone: ${item.phone} | Email: ${item.email} `;
+
+            const btnDelete = document.createElement('button');
+            btnDelete.textContent = "Delete";
+            btnDelete.addEventListener('click', () => deleteContact(key));
+
+            const btnEdit = document.createElement('button');
+            btnEdit.textContent = "Edit";
+            btnEdit.addEventListener('click', () => editContact(key));
+
+            line.appendChild(btnDelete);
+            line.appendChild(btnEdit);
+
+            ulPeoples.appendChild(line);
+        }
+    });    
+}
+
+   function deleteContact(id) {
+    if (confirm("Are you sure you want to delete this contact?")) {
+        form.reset();
+        list.splice(id, 1);
+        saveContact();
+        listing();
+        alert("Contact deleted successfully.");
+    }};
+
+
+    function saveContact() {
+        localStorage.setItem("mylist", JSON.stringify(list));
+    };
+
+    function editContact(id) {
+        form.id.value = id;
+        form.name.value = list[id].name;
+        form.phone.value = list[id].phone;
+        form.email.value = list[id].email;
+    };
+
