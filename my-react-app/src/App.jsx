6a2f5a4fd0be830ce23994f14daa05c305ad75fb
Nikolas Konoski <nikolasmkonoski@gmail.com>
2025-05-21 23:04:04 -0300

    refactoring for react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <header className="header">
+        <h1>Minha Logo</h1>
+      </header>
+      <p>Conteúdo do site</p>
+    </div>
+  );
+}
+
+export default App;
